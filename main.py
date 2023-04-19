216a8982c3dd6e2358abf0c94dba1765df5c573d
shirarozenthal <112002091+shirarozenthal@users.noreply.github.com>
2023-04-19 00:02:03 -0500

    Final

--- main.py
+++ main.py
@@ -14,16 +14,48 @@ def MED(S, T):
         if (S[0] == T[0]):
             return(MED(S[1:], T[1:]))
         else:
-            return(1 + min(MED(S, T[1:]), MED(S[1:], T)))
+            return(1 + min(MED(S, T[1:]), MED(S[1:], T), MED(S[1:], T[1:])))
 
 
 def fast_MED(S, T, MED={}):
     # TODO -  implement memoization
-    pass
+    if (S, T) in MED:
+        return MED[(S, T)]
+    if (S == ""):
+        ret = len(T)
+    elif (T == ""):
+        ret = len(S)
+    elif (S[0] == T[0]):
+        ret = fast_MED(S[1:], T[1:], MED)
+    else:
+        ret = 1 + min(fast_MED(S, T[1:], MED), 
+                      fast_MED(S[1:], T, MED),  
+                      fast_MED(S[1:], T[1:], MED))
+    MED[(S, T)] = ret
+    return ret
+
 
 def fast_align_MED(S, T, MED={}):
     # TODO - keep track of alignment
-    pass
+    if (T == ""):
+        ret = (S, '-' * len(S))
+    elif (S == ""):
+        ret = ('-' * len(T), T)
+    elif (S[0] == T[0]):
+        aS, aT = fast_align_MED(S[1:], T[1:], MED)
+        ret = (S[0] + aS, T[0] + aT)
+    else:
+        minimum = min((fast_MED(S[1:], T[1:], MED)), (fast_MED(S, T[1:], MED)), (fast_MED(S[1:], T, MED)))
+        if minimum == (fast_MED(S[1:], T[1:], MED)):
+            aS, aT = fast_align_MED(S[1:], T[1:], MED)
+            ret = (S[0] + aS, T[0] + aT)
+        elif minimum == (fast_MED(S, T[1:], MED)):
+            aS, aT = fast_align_MED(S, T[1:], MED)
+            ret = ('-' + aS, T[0] + aT)
+        else:
+            aS, aT = fast_align_MED(S[1:], T, MED)
+            ret = (S[0] + aS, '-' + aT)
+    return ret
 
 def test_MED():
     for S, T in test_cases:
